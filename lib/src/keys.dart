589052cb50b149e94bbd6a4dfaa2579d2abcd305
yurydemin <yury.demin@instatsport.com>
2021-05-02 00:32:23 +0500

    add movies list view

--- lib/src/keys.dart
+++ lib/src/keys.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MoviesAppKeys {
+  static const moviesList = Key('moviesListKey');
   static const tabs = Key('tabsKey');
   static const moviesTab = Key('moviesTabKey');
   static const favoritesTab = Key('favoritesTabKey');
--- lib/src/widgets/movies_list.dart
+++ lib/src/widgets/movies_list.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:bloc_movies_gallery/src/blocs/movies/movies_bloc.dart';
+import 'package:bloc_movies_gallery/src/blocs/movies/movies_event.dart';
+import 'package:bloc_movies_gallery/src/blocs/movies/movies_state.dart';
+import 'package:bloc_movies_gallery/src/keys.dart';
+import 'package:bloc_movies_gallery/src/screens/movie_details_screen.dart';
+import 'package:bloc_movies_gallery/src/widgets/movie_item.dart';
 
 class MoviesList extends StatelessWidget {
   const MoviesList({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<MoviesBloc, MoviesState>(
+      builder: (context, state) {
+        if (state is MoviesLoadInProgressState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is MoviesLoadSuccessState) {
+          final movies = state.movies;
+          return ListView.builder(
+            key: MoviesAppKeys.moviesList,
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final movie = movies[index];
+              return MovieItem(
+                movie: movie,
+                onTap: () async {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (_) {
+                      return MovieDetailsScreen(movieId: movie.id);
+                    }),
+                  );
+                },
+                onFavoriteTap: () {
+                  BlocProvider.of<MoviesBloc>(context).add(
+                    MovieToogleFavoriteEvent(
+                        movie.copyWith(isFavorite: !movie.isFavorite)),
+                  );
+                },
+              );
+            },
+          );
+        } else {
+          return Container(
+            color: Colors.black,
+          );
+        }
+      },
+    );
   }
 }
